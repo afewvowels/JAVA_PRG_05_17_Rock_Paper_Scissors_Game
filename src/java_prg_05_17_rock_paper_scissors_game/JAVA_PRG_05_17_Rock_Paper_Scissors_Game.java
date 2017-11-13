c3eb24d7aabea1fbf6d645162d41197ca7c6b728
Keith Smith <32456218+afewvowels@users.noreply.github.com>
2017-11-12 19:23:12 -0500

    Tested and working.

--- src/java_prg_05_17_rock_paper_scissors_game/JAVA_PRG_05_17_Rock_Paper_Scissors_Game.java
+++ src/java_prg_05_17_rock_paper_scissors_game/JAVA_PRG_05_17_Rock_Paper_Scissors_Game.java
@@ -5,6 +5,8 @@
  */
 package java_prg_05_17_rock_paper_scissors_game;
 
+import java.util.Scanner;
+import java.util.Random;
 /**
  *
  * @author bluebackdev
@@ -39,8 +41,100 @@ public class JAVA_PRG_05_17_Rock_Paper_Scissors_Game {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args)
+    {
+        int intMenuSelection = -1;
+        int intMoveComputer;
+        int intMovePlayer;
+        
+        do
+        {
+            intMoveComputer = generateComputerMove();
+            
+            intMovePlayer = displayMenu();
+            
+            determineWinner(intMoveComputer, intMovePlayer);
+            
+        } while (intMovePlayer != 4);
     }
     
+    public static int generateComputerMove()
+    {
+        int intComputerChoice;
+        
+        Random randomMove = new Random();
+        
+        intComputerChoice = randomMove.nextInt(3) + 1;
+        
+        return intComputerChoice;
+    }
+    
+    public static int displayMenu()
+    {
+        int intChoicePlayer;
+        
+        Scanner scrKeyboard = new Scanner(System.in);
+        
+        do
+        {
+            System.out.println("Please select a move from the following menu options:");
+            System.out.println("\t1. Rock");
+            System.out.println("\t2. Paper");
+            System.out.println("\t3. Scissors");
+            System.out.println("\t4. Quit");
+
+            intChoicePlayer = scrKeyboard.nextInt();
+        } while (intChoicePlayer != 1 && intChoicePlayer != 2 &&
+                intChoicePlayer != 3 && intChoicePlayer != 4);
+        
+        return intChoicePlayer;
+    }
+    
+    public static void determineWinner(int intChoiceComputer, int intChoicePlayer)
+    {
+        if ((intChoiceComputer == 1 && intChoicePlayer == 1) ||
+                (intChoiceComputer == 2 && intChoicePlayer == 2) ||
+                (intChoiceComputer == 3 && intChoicePlayer == 3))
+        {
+            System.out.println("There was a tie");
+        }
+        else if (intChoiceComputer == 1 && intChoicePlayer == 2)
+        {
+            System.out.println("Computer chooses rock, player chooses paper");
+            System.out.println("Player wins");
+        }
+        else if (intChoiceComputer == 1 && intChoicePlayer == 3)
+        {
+            System.out.println("Computer chooses rock, player chooses scissors");
+            System.out.println("Computer wins");
+        }
+        else if (intChoiceComputer == 2 && intChoicePlayer == 1)
+        {
+            System.out.println("Computer chooses paper, player chooses rock");
+            System.out.println("Computer wins");
+        }
+        else if (intChoiceComputer == 2 && intChoicePlayer == 3)
+        {
+            System.out.println("Computer chooses paper, player chooses scissors");
+            System.out.println("Player wins");
+        }
+        else if (intChoiceComputer == 3 && intChoicePlayer == 1)
+        {
+            System.out.println("Computer chooses scissors, player chooses rock");
+            System.out.println("Player wins");
+        }
+        else if (intChoiceComputer == 3 && intChoicePlayer == 2)
+        {
+            System.out.println("Computer chooses scissors, player chooses paper");
+            System.out.println("Computer wins");
+        }
+        else if (intChoicePlayer == 4)
+        {
+            System.out.println("Thank you for playing");
+        }
+        else
+        {
+            System.out.println("ERROR DETERMINING WINNER");
+        }
+    }
 }
