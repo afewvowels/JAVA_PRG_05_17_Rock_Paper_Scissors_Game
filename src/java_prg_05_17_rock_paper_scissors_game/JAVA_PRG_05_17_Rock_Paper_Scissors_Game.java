2656ec119d6151b1bdd363c1717b7045bd8da9ae
Keith Smith <32456218+afewvowels@users.noreply.github.com>
2017-11-12 19:06:12 -0500

    Prompt written.

---
+++ src/java_prg_05_17_rock_paper_scissors_game/JAVA_PRG_05_17_Rock_Paper_Scissors_Game.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package java_prg_05_17_rock_paper_scissors_game;
+
+/**
+ *
+ * @author bluebackdev
+ * Write a program that lets the user play the game of Rock, Paper, Scissors
+ * against the computer. The program should work as follows:
+ * 
+ * 1. When the program begins, a random number in the range of 1 through 3 is
+ * generated. If the number is 1, then the computer has chosen rock. If the
+ * number is 2, then the computer has chosen paper. If the number is 3, then
+ * the computer has chosen scissors. (Don't display the computer's choice yet)
+ * 
+ * 2. The user enters his or her choice of "rock", "paper", or "scissors" at
+ * the keyboard. (You can use a menu if you prefer.)
+ * 
+ * 3. The computer's choice is displayed.
+ * 
+ * 4. A winner is selected according to the following rules:
+ * 
+ *      * If one player chooses rock and the other player chooses scissors,
+ *        then rock wins. (The rock smashes scissors.)
+ *      * If one player chooses scissors and the other player chooses paper,
+ *        then scissors wins. (Scissors cuts paper.)
+ *      * If one player chooses paper and the other player chooses rock,
+ *        then paper wins. (Paper wraps rock.)
+ *      * If both players make the same choice, the game must be played again
+ *        to determine the winner.
+ * 
+ * Be sure to divide the program into methods that perform each major task.
+ */
+public class JAVA_PRG_05_17_Rock_Paper_Scissors_Game {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
